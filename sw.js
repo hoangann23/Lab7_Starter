18149d03ddb7736f042cc6c9c20253297073d10b
Ann Luong <annluong146@gmail.com>
2022-11-10 00:10:56 -0800

    async

--- sw.js
+++ sw.js
@@ -42,7 +42,7 @@ self.addEventListener('fetch', function (event) {
   /*******************************/
   // B7. TODO - Respond to the event by opening the cache using the name we gave
   //            above (CACHE_NAME)
-  event.respondWith(caches.open(CACHE_NAME).then((cache) => {
+  event.respondWith(caches.open(CACHE_NAME).then(async (cache) => {
     return cache.match(event.request).then((cachedResponse) => {
       return cachedResponse || fetch(event.request).then((fetchedResponse) => {
         cache.put(event.request, fetchedResponse.clone());
